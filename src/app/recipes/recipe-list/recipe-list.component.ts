2c2b4747f75e3de13e5d462bf0d6de95450a910a
Paweł Srebrny <psrebrny@one2tribe.pl>
2017-05-06 14:01:51 +0200

    data and event binding between components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,17 +7,23 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://goo.gl/7mnwNx'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://goo.gl/7mnwNx'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://goo.gl/7mnwNx'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://goo.gl/7mnwNx')
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://goo.gl/7mnwNx'),
+    new Recipe('Third Test Recipe', 'This is simply a test', 'https://goo.gl/7mnwNx'),
+    new Recipe('May the Fourth will be with you', 'This is simply a test', 'https://goo.gl/7mnwNx')
   ];
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
